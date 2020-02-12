bb5f01136fb075d23c28ebd6cfa9121c88b7ebf2
Misaki Koonce <misaki.koonce@gmail.com>
2020-02-12 11:13:56 -0800

    Add Header.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import './App.css'
+import Header from './Header.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Yo Yo!</h1>
-        <h1>Mock of Tesla website</h1>
-      </header>
+    <div>
+      <Header />
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,2 +1,26 @@
 import React from 'react'
 import { Route, Link } from 'react-router-dom'
+
+const Header = () => {
+    return(
+        <nav>
+            <ol>
+                <li><a>Model S</a></li>
+                <li><a>Model 3</a></li>
+                <li><a>Model X</a></li>
+                <li><a>Model Y</a></li>
+                <li><a>CyberTruck</a></li>
+                <li><a>Roadster</a></li>
+                <li><a>Energy</a></li>
+                <li>
+                    <ol>
+                        <li><a>Shop</a></li>
+                        <li><a>Sign In</a></li>
+                    </ol>
+                </li>
+            </ol>
+        </nav>
+    )
+}
+
+export default Header
